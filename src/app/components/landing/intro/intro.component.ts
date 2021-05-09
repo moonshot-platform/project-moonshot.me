4cc7f165b94ad667eb1b019dafddd0c04c05c8f0
Tolga <tolga@trivetechnology.com>
2021-05-09 08:11:40 +0800

    improved the remaining pages with the new design layout

--- src/app/components/landing/intro/intro.component.ts
+++ src/app/components/landing/intro/intro.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Container, Main, ISourceOptions, tsParticles } from 'ng-particles';
-declare let particlesJS: any;
 
 @Component({
   selector: 'app-intro',
@@ -9,21 +7,8 @@ declare let particlesJS: any;
 })
 export class IntroComponent implements OnInit {
 
-  constructor() {
-    particlesJS.load('particles', 'assets/json/particlesjs-config.json', function () { });
-  }
+  constructor() {}
 
   ngOnInit(): void {
-    tsParticles.loadJSON('particles2', 'assets/json/particlesjs-config-yellow-balls.json');
-  }
-
-  particlesLoaded(container: Container): void {
-    console.log(container);
-  }
-
-  particlesInit(main: Main): void {
-    console.log(main);
-
-    // Starting from 1.19.0 you can add custom presets or shape here, using the current tsParticles instance (main)
   }
 }
--- src/app/components/landing/tokenomics/tokenomics.component.ts
+++ src/app/components/landing/tokenomics/tokenomics.component.ts
@@ -8,34 +8,38 @@ import { Component, OnInit } from '@angular/core';
 export class TokenomicsComponent implements OnInit {
 
   public list: any = [
-    {
-      key: "total supply:",
-      val: "1,000,000,000,000,000 (1 quadrillion)"
-    },
-    {
-      key: "circulating supply:",
-      val: "000,000,000"
-    },
-    {
-      key: "burned forever:",
-      val: "000,000,000"
-    },
-    {
-      key: "price for one moonshot:",
-      val: "000,000,000"
-    },
-    {
-      key: "price for one million moonshot:",
-      val: "000,000,000"
-    },
-    {
-      key: "moonshot for 1 bnb:",
-      val: "1 bnb = 000,000,000 moonshot"
-    },
-    {
-      key: "market cap:",
-      val: "000,000,000"
-    },
+    [
+      {
+        key: "total supply:",
+        val: "1,000,000,000,000,000 (1 quadrillion)"
+      },
+      {
+        key: "circulating supply:",
+        val: "000,000,000"
+      },
+      {
+        key: "burned forever:",
+        val: "000,000,000"
+      },
+      {
+        key: "price for one moonshot:",
+        val: "000,000,000"
+      },
+    ],
+    [
+      {
+        key: "price for one million moonshot:",
+        val: "000,000,000"
+      },
+      {
+        key: "moonshot for 1 bnb:",
+        val: "1 bnb = 000,000,000 moonshot"
+      },
+      {
+        key: "market cap:",
+        val: "000,000,000"
+      },
+    ]
   ]
 
   constructor() { }
