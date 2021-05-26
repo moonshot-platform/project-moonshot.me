68f6c0c1a46e307ff097d0caac944bf60f2320d8
S1nPur1ty <singular.purity@gmail.com>
2021-05-26 13:01:07 +0800

    added the how to buy flow

--- src/app/components/landing/tutorial/tutorial.component.ts
+++ src/app/components/landing/tutorial/tutorial.component.ts
@@ -7,6 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TutorialComponent implements OnInit {
 
+  steps: any = [
+    {
+      indicator: 'Counting down'
+    },
+    {
+      title: 'Download a browser based wallet',
+      description: 'Moonshot is a token running on the Binance Smart Chain Network. You can use Metamask, TrustWallet, Binance Smart Chain Wallet and others. Accept warning and go on V1.',
+    },
+    {
+      title: 'Buy BNB',
+      description: 'Use an exchange to buy BNB. Some exchanges are Binance, Binance US.',
+    },
+    {
+      title: 'Go to Moonshot website',
+      description: 'When you visit Moonshot, click on \'Buy on PancakeSwap\' button. This will take you to the PancakeSwap website. ',
+    },
+    {
+      title: 'Import the token address',
+      description: 'The PancakeSwap website starts with a pop up form asking if the address belongs to Moonshot. Double check before you accept! 0xd27d3f7f329d93d897612e413f207a4dbe8bf799',
+    },
+    {
+      title: 'Set slippage to 11%',
+      description: 'Click on the gear button and set slippage to 11%. (the slippage covers the 10% explained in tokenomics) If that gives you an error, increase the slippage to 12% or more.',
+    },
+    {
+      title: 'Swap BNB for Moonshot',
+      description: 'Confirm with your browser based wallet.',
+    },
+    {
+      title: 'View Moonshot in your wallet',
+      description: 'To view your Moonshot, add custom token to your browser based wallet. Use the address above.',
+    },
+    {
+      indicator: 'Lift-off',
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
