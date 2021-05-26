d2bf9d322449b1f026318acc33f6e8d45ae2c8df
Clover Dev <cloverdev88@gmail.com>
2021-05-26 03:37:40 -0400

    implemented the news section

--- src/app/components/landing/news/news.component.ts
+++ src/app/components/landing/news/news.component.ts
@@ -7,6 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsComponent implements OnInit {
 
+  public imgSrcList: any = [
+    {
+      src: "",
+      srcOut: "assets/media/images/news/github_icon_yellow.svg",
+      srcOn: "assets/media/images/news/github_icon.svg",
+      alt: "github"
+    },
+    {
+      src: "",
+      srcOut: "assets/media/images/news/twitter_icon_yellow.svg",
+      srcOn: "assets/media/images/news/twitter_icon.svg",
+      alt: "twitter"
+    },
+    {
+      src: "",
+      srcOut: "assets/media/images/news/telegram_icon_yellow.svg",
+      srcOn: "assets/media/images/news/telegram_icon.svg",
+      alt: "telegram"
+    },
+    {
+      src: "",
+      srcOut: "assets/media/images/news/discord_icon_yellow.svg",
+      srcOn: "assets/media/images/news/discord_icon.svg",
+      alt: "discord"
+    },
+    {
+      src: "",
+      srcOut: "assets/media/images/news/reddit_icon_yellow.svg",
+      srcOn: "assets/media/images/news/reddit_icon.svg",
+      alt: "reddit"
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
