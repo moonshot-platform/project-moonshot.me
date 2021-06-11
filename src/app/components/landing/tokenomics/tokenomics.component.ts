c531b18975391b76b618d4f978679e138af075f8
sagar <sagbhujbal2211@gmail.com>
2021-06-11 18:53:19 +0530

    tokenomics_change

--- src/app/components/landing/tokenomics/tokenomics.component.ts
+++ src/app/components/landing/tokenomics/tokenomics.component.ts
@@ -11,34 +11,41 @@ export class TokenomicsComponent implements OnInit {
     [
       {
         key: "total supply:",
-        val: "1,000,000,000,000,000 (1 quadrillion)"
+        val: "1,000,000,000,000,000",
+        shortVal: "(1 quadrillion)"
       },
       {
         key: "circulating supply:",
-        val: "000,000,000"
+        val: "607,374,578,320,453",
+        shortVal: ""
       },
       {
         key: "burned forever:",
-        val: "000,000,000"
-      },
-      {
-        key: "price for one moonshot:",
-        val: "000,000,000"
+        val: "392,625,581,608,018",
+        shortVal: ""
       },
     ],
     [
       {
-        key: "price for one million moonshot:",
-        val: "000,000,000"
-      },
-      {
         key: "moonshot for 1 bnb:",
-        val: "1 bnb = 000,000,000 moonshot"
+        val: "15,117,475,090",
+        shortVal: ""
       },
       {
         key: "market cap:",
-        val: "000,000,000"
+        val: "$23,918,890",
+        shortVal: ""
       },
+      {
+        key: "price for 1 million moonshot:",
+        val: "$0.03935…",
+        shortVal: ""
+      },
+      {
+        key: "price for 1 moonshot:",
+        val: "$0.00000004219…",
+        shortVal: ""
+      }
     ]
   ]
 
