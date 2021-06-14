ea2ecffef12193cc620879e2977da8c964c8909d
Tolga <tolga@trivetechnology.com>
2021-06-15 06:17:26 +0800

    footer buttons fix

--- src/app/components/community/community.component.ts
+++ src/app/components/community/community.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-community',
@@ -9,7 +11,22 @@ export class CommunityComponent {
 
   static readonly routeName: string = 'community';
 
-  constructor() {
+  private fragment: string;
+
+  constructor(private route: ActivatedRoute, private location: Location, private _router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.route.fragment.subscribe(fragment => { 
+      this.fragment = fragment; 
+      this.location.replaceState('/community'); 
+    });
+  }
+
+  ngAfterViewInit(): void {
+    try {
+      document.querySelector('#' + this.fragment).scrollIntoView();
+    } catch (e) { }
   }
 
 }
