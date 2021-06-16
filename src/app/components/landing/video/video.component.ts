91354e42fd94c5d4587836156fe633f70b6afb35
S1nPur1ty <singular.purity@gmail.com>
2021-06-16 11:18:31 +0800

    added better video feature in the home page

--- src/app/components/landing/video/video.component.ts
+++ src/app/components/landing/video/video.component.ts
@@ -1,3 +1,4 @@
+import { OnDestroy } from '@angular/core';
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
@@ -5,26 +6,61 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
   templateUrl: './video.component.html',
   styleUrls: ['./video.component.scss']
 })
-export class VideoComponent implements OnInit {
+export class VideoComponent implements OnInit, OnDestroy {
   public videoSource: string = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
 
   @ViewChild('videoPlayer') public videoPlayer: ElementRef<HTMLVideoElement>;
-  public paused = false
-  public videoPlaying = false
+  public isPlaying = false;
+  public show = true;
+  public fadeOut = false;
+  public timeoutRunning = false;
+  private timeout: any;
 
   constructor() { }
 
   toggleVideo(): void {
     if(this.videoPlayer.nativeElement.paused) {
       this.videoPlayer.nativeElement.play();
-      this.videoPlaying = true;
     } else {
       this.videoPlayer.nativeElement.pause();
-      this.videoPlaying = false;
+    }
+
+    this.isPlaying = !this.isPlaying;
+
+    if( this.isPlaying ) {
+      this.fadeOut = true;
+      this.timeoutRunning = true;
+      this.timeout = setTimeout( () => this.onFadeOut(), 700 );
+    } else {
+      this.show = true;
+    }
+    
+  }
+
+  onFadeOut() {
+    this.timeoutRunning = false;
+    this.show = false;
+    this.fadeOut = false;
+  }
+
+  onShow( onHover: boolean ): void {
+    if( onHover )
+      this.show = true;
+    else {
+      if( this.timeoutRunning ) {
+        clearTimeout(this.timeout);
+        this.onFadeOut();
+      }
+      
+      this.show = this.isPlaying ? false : true;
     }
   }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    if( this.timeoutRunning ) clearTimeout(this.timeout);
   }
 
 }
