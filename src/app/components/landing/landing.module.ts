29345156e4cdd13c9ebc36033bb924a0401b5d00
S1nPur1ty <singular.purity@gmail.com>
2021-06-16 19:45:38 +0800

    improved sidebar and tokenomics

--- src/app/components/landing/landing.module.ts
+++ src/app/components/landing/landing.module.ts
@@ -15,7 +15,7 @@ import { InvestmentComponent } from './investment/investment.component';
 import { TutorialComponent } from './tutorial/tutorial.component';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { NavigationModule } from '../base/navigation/navigation.module';
-import { SidebarModule } from './sidebar/sidebar.module';
+import { SidebarModule } from '../base/sidebar/sidebar.module';
 
 import { CarouselModule } from 'ng-carousel-cdk';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -26,7 +26,6 @@ import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { TokenomicsModule } from './tokenomics/tokenomics.module';
 
 
 
@@ -61,7 +60,6 @@ import { TokenomicsModule } from './tokenomics/tokenomics.module';
     FormsModule,
     ReactiveFormsModule,
     CarouselModule,
-    TokenomicsModule
   ]
 })
 export class LandingModule { }
