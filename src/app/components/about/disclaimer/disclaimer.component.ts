77669850f9e2f1e6d50465d2de4cf1244a656398
S1nPur1ty <singular.purity@gmail.com>
2021-06-17 13:52:57 +0800

    added disclaimer to the about page

---
+++ src/app/components/about/disclaimer/disclaimer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-disclaimer',
+  templateUrl: './disclaimer.component.html',
+  styleUrls: ['./disclaimer.component.scss']
+})
+export class DisclaimerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
