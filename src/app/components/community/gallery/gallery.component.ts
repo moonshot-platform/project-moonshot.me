da020f219f67ced51705e172f5ce82664d02681e
Clover Dev <cloverdev88@gmail.com>
2021-06-17 05:48:53 -0400

    Implemented Gallery in Community page

--- src/app/components/community/gallery/gallery.component.ts
+++ src/app/components/community/gallery/gallery.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
+  current = 0;
+
+  public items: any = [
+    'assets/media/images/community/ga1.png',
+    'assets/media/images/community/ga2.png',
+    'assets/media/images/community/ga3.png'
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  next() {
+    this.current = this.current < this.items.length - 1 ? this.current + 1 : 0;
+  }
+
+  prev() {
+      this.current = this.current > 0 ? this.current - 1 : this.items.length - 1;
   }
 
 }
