6f44ee8002250ff899180e890917082e6b63e962
furkanksl <furkankoseoglu65@gmail.com>
2021-06-18 11:12:17 +0300

    added meta tags for about page

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Location } from '@angular/common';
+import { Meta,Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about',
@@ -13,8 +14,23 @@ export class AboutComponent {
 
   private fragment: string;
 
-  constructor(private route: ActivatedRoute, private location: Location, private _router: Router) {
-  }
+  constructor(
+    private route: ActivatedRoute, 
+    private location: Location,
+    private _router: Router,
+    private meta: Meta, // For Meta tags
+    private title: Title // For Title of page
+    ){
+      this.meta.addTags([
+        {name: 'description', content: 'An ambitious, exploratory, community driven experiment in the DeFi space'},
+        {name: 'author', content: 'moonshot'},
+        {name: 'keywords', content: 'Moonshot, About Moonshot, Vision and Mission, Moonshot Mechanics, Security, What\'s Next'}
+      ]);
+      this.setTitle('About Moonshot'); // i do not know it is necessary.
+    }
+    public setTitle( newTitle: string) {
+      this.title.setTitle( newTitle );
+    }
 
   ngOnInit(): void {
     this.route.fragment.subscribe(fragment => { 
