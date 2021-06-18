45ea0de53ddd0ff631c2e1b1ce041726bf7c5c61
furkanksl <furkankoseoglu65@gmail.com>
2021-06-18 11:22:41 +0300

    added meta tags for community page

--- src/app/components/community/community.component.ts
+++ src/app/components/community/community.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Location } from '@angular/common';
+import { Meta,Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-community',
@@ -13,8 +14,23 @@ export class CommunityComponent {
 
   private fragment: string;
 
-  constructor(private route: ActivatedRoute, private location: Location, private _router: Router) {
-  }
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private _router: Router,
+    private meta: Meta,
+    private title: Title,
+    ) {
+      this.meta.addTags([
+        {name: 'description', content: 'The endless energy and creativity of our community and the desire to help each other to reach project milestones is our biggest strength and motivator. Join the #MoonSquad and be part of the Moonshot community'},
+        {name: 'author', content: 'moonshot'},
+        {name: 'keywords', content: 'Moonshot, Moonshot Community, MoonTV, MoonTicket, MoonGallery, moonshot, moonshot community, moontv, moongallery, moonticket, moon tv, moon gallery, moon ticket'}
+      ]);
+      this.setTitle('Moonshot Comunity');
+    }
+    public setTitle( newTitle: string) {
+      this.title.setTitle( newTitle );
+    }
 
   ngOnInit(): void {
     this.route.fragment.subscribe(fragment => { 
