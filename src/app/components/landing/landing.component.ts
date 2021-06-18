ee376d897febebec36fd79fd61a518676a6fbb6f
furkanksl <furkankoseoglu65@gmail.com>
2021-06-18 11:30:29 +0300

    added meta tags for Landing page

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { Meta,Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-landing',
@@ -13,8 +14,22 @@ export class LandingComponent implements OnInit {
 
   private fragment: string;
 
-  constructor(private route: ActivatedRoute, private location: Location) {
-  }
+  constructor(
+    private route: ActivatedRoute, 
+    private location: Location,
+    private meta: Meta,
+    private title: Title,
+    ) {
+      this.meta.addTags([
+        {name: 'description', content: 'Moonshot is a deflationary, frictionless yield and Liquidity protocol. Read more about Moonshot or start buying!'},
+        {name: 'author', content: 'moonshot'},
+        {name: 'keywords', content: 'Moonshot, How To Buy Moonshot,Moonshot\'s Partners, Moonshot Community, MoonTicket, MoonPaper, MoonTV, Tokenomics, Merchandise, MoonGallery, About Moonshot, Fuel the rocket, moonshot, how to buy moonshot, moonshot partners, moonshot community, moonticket, moonpaper'}
+      ]);
+      this.setTitle('Moonshot');
+    }
+    public setTitle( newTitle: string) {
+      this.title.setTitle( newTitle );
+    }
 
   ngOnInit(): void {
     this.route.fragment.subscribe(fragment => { 
