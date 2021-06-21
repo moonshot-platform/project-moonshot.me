7087177c9c105c7dd7c17d6bad0fb552f43adebc
furkanksl <furkankoseoglu65@gmail.com>
2021-06-21 23:38:54 +0300

    trying to fix meta image issue for whatsapp

--- src/app/components/community/community.component.ts
+++ src/app/components/community/community.component.ts
@@ -26,26 +26,18 @@ export class CommunityComponent {
         {name: 'author', content: 'moonshot'},
         //{name: 'keywords', content: 'Moonshot, Moonshot Community, MoonTV, MoonTicket, MoonGallery, moonshot, moonshot community, moontv, moongallery, moonticket, moon tv, moon gallery, moon ticket'}
         {name:'twitter:card', content:'summary'},
-        {name:'twitter:description', content:'Watch MoonTV - Zap to your favourite channel and watch some Moonshot crypto'},
+        {name:'twitter:description', content:'Moonshot is a deflationary, frictionless yield and liquidity generation protocol.'},
         {name:'twitter:title', content:'MoonTV'},
         {name:'twitter:image', content:'./src/assets/media/images/moonshot-logo.png'},
         {name:'twitter:site', content:'@RS25Moonshot'},
 
         {property:'og:title', content:'Project Moonshot'},
         {property:'og:site_name', content:'Moonshot'},
-        {property:'og:description', content:'MoonTV - Zap to your favourite channel'},
-        {property:'og:image', content:'./src/assets/media/images/moonshot-logo.png'},     
-/* 
-        {property:'og:type', content:'website'},
-        {property:'og:site_name', content:'Moonshot'},
-        {property:'og:title', content:'Project Moonshot'},
-        {property:'og:description', content:'Watch MoonTV - Zap to your favourite channel and watch some Moonshot crypto'},
-        {property:'og:url', content:'https://t.me/MoonShotChat'},
-        {property:'og:locale', content:'en_US'},
-        {property:'og:image', content:'https://cdn4.telesco.pe/file/WB_mshLvhrX1lDICQ_oYE_8zcAS_gse7Oz7EeSMZHMyOiLYJ3QloLfPuUeRDkobvrk6eDoc9DzRHu6Ct2JrrotdUfnH7xBIzyVlHS53InqEKKUpqw4202ccEvr8LVfoadOHMo-Lbmu6fULjK9dk6xt8pnGAPitxtBP_NQjKDwAlR7PV7OsPdohlplsuWzWAuusPBP0kZD2ZV5Z7hHiUCKS2YzDfSYHqKXliUe1MnKG7XOjEj05Cy5ZRh1QtWDsWiJsDwLHNlzKEIBwCbYTS-06xouZwghEiJMB8mSn3HeNsbIa6b3THJeCELtquzalT1Ee7Z_-_O9kRP0lzYnGqj2A.jpg'},
- */
+        {property:'og:description', content:'Moonshot is a deflationary, frictionless yield and liquidity generation protocol.'},
+        {property:'og:image', itemprop:'image', content:'./src/assets/media/images/moonshot-logo.png'},     
+
       ]);
-      this.setTitle('Moonshot Comunity');
+      this.setTitle('Moonshot Community');
     }
     public setTitle( newTitle: string) {
       this.title.setTitle( newTitle );
