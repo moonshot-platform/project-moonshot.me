091e92de10a42f45eef67a1d96d1dde000ce3461
Tolga <tolga@trivetechnology.com>
2021-07-06 15:32:56 +0800

    added pixijs core files

--- src/app/components/games/shooter/shooter.component.ts
+++ src/app/components/games/shooter/shooter.component.ts
@@ -17,7 +17,7 @@ export class ShooterComponent implements OnInit {
 
   @Input()
   public applicationOptions: IApplicationOptions = {
-    transparent: true,
+    transparent: false,
 		antialias: true,
 		resolution: this.devicePixelRatio,
     autoDensity: true,
@@ -36,6 +36,35 @@ export class ShooterComponent implements OnInit {
     
     document.getElementById('pixi-container').appendChild(this.app.view);
     this.resize();
+    this.onRender();
+  }
+
+  private bunny: Sprite;
+
+  onRender(): void {
+    this.app.view.style.transformOrigin = `top left`;
+
+    this.bunny = Sprite.from('assets/media/images/moonshot-logo.png');
+
+    // center the sprite's anchor point
+    this.bunny.anchor.set(0.5);
+
+    this.app.stage.addChild(this.bunny);
+
+    this.app.ticker.add(() => {
+        // just for fun, let's rotate mr rabbit a little
+        this.bunny.rotation += 0.1;
+    });
+
+    this.repositionAssets();
+  }
+
+  repositionAssets(): void {
+    if(this.bunny !== undefined) {
+      // move the sprite to the center of the screen
+      this.bunny.x = this.app.screen.width / 2;
+      this.bunny.y = this.app.screen.height / 2;
+    }
   }
 
   @HostListener('window:resize')
@@ -44,14 +73,15 @@ export class ShooterComponent implements OnInit {
     const height = document.getElementById('pixi-container').offsetHeight;
     const viewportScale = 1 / this.devicePixelRatio;
     
-    this.app.renderer.resize(width * this.devicePixelRatio, height * this.devicePixelRatio);
-    this.app.view.style.transform = `scale(${viewportScale})`;
-    this.app.view.style.transformOrigin = `top left`;
+    this.app.renderer.resize(
+      width * viewportScale, 
+      height * viewportScale
+    );
+    // this.app.view.style.transform = `scale(${viewportScale})`;
+    this.app.view.style.width = `100%`;
+    this.app.view.style.height = `100%`;
 
-    const sprite: Sprite = Sprite.from('assets/media/images/moonshot-logo.png');
-    this.app.stage.addChild(sprite);
-
-    this.app.render();
+    this.repositionAssets();
   }
 
   ngOnDestroy(): void {
