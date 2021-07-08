f05f7c8e6f872ccfa596442ffe80ae9fd7ec2fbe
furkanksl <furkankoseoglu65@gmail.com>
2021-07-09 00:56:31 +0300

    fixed load more button margin top issue

--- src/app/components/about/team/members/members.component.ts
+++ src/app/components/about/team/members/members.component.ts
@@ -83,6 +83,85 @@ export class MembersComponent implements OnInit {
       title : 'Solidity Developer',
       about : 'Experienced backend engineer turned solidity developer. Enthusiast of blockchain and smart contract.'
       
+    }
+  ];
+
+
+  allMembers : any = [
+    {
+      name : 'c0ntrol zer0',
+      image : 'assets/media/images/members/c0ntrolzer0.png',
+      title : 'Main Developer',
+      about : 'Software Engineer, SCRUM master and guardian of the keys.'
+    },
+    {
+      name : 'luisqsm',
+      image : 'assets/media/images/members/luisqsm.jpg',
+      title : 'Design & Brand Manager',
+      about : 'Architect, 3D artist, digital designer & crypto enthusiast.',
+      social : [
+        {
+          link: 'https://twitter.com/luisqsm2',
+          icon: 'assets/media/icons/social/twitter-yellow-round-icon.svg',
+          alt: 'twitter-icon'
+        }
+      ]
+    },
+    {
+      name : 'Smack',
+      image : 'assets/media/images/members/smack.jpg',
+      title : 'Positivity Guru',
+      about : 'Connecting the people, the dots, to one piece of art.',
+      social :[
+        {
+          link: 'https://twitter.com/smack_tm',
+          icon: 'assets/media/icons/social/twitter-yellow-round-icon.svg',
+          alt: 'twitter-icon'
+        },
+        {
+          link: 'https://t.me/Bornlucky89',
+          icon: 'assets/media/icons/social/telegram-yellow-round-icon.svg',
+          alt: 'telegram-icon'
+        }
+      ]
+    },
+    {
+      name : 'Winnie',
+      image : 'assets/media/images/members/winnie.jpg',
+      title : 'Moderator/Contributor',
+      about : 'Running MoonSquad merchandise.'
+    },
+    {
+      name : 'Clover',
+      image : 'assets/media/images/members/clover.jpg',
+      title : 'Full stack expert',
+      about : 'Passionate and experienced frontend/backend/blockchain developer.'
+    },
+    {
+      name : 'CryptoKam',
+      image : 'assets/media/images/members/cryptokam.jpeg',
+      title : 'Moderator/Contributor',
+      about : 'Marketing advisor.<br>Creative ideas support.<br>Shill trooper.<br>'
+    },
+    {
+      name : 'CryptoHunter',
+      image : 'assets/media/images/members/cryptohunter.jpg',
+      title : 'Moderator/Marketeer', //'CryptoHunter, [Moderator/Marketeer]',
+      about : 'Digital Marketing SMO (Youtube, Instagram).',
+      social : [
+        {
+          link: 'https://twitter.com/cryptomshot/',
+          icon: 'assets/media/icons/social/twitter-yellow-round-icon.svg',
+          alt: 'twitter-icon'
+        }
+      ]
+    },
+    {
+      name : 'Sonic',
+      image : 'assets/media/images/members/sonic.jpg',
+      title : 'Solidity Developer',
+      about : 'Experienced backend engineer turned solidity developer. Enthusiast of blockchain and smart contract.'
+      
     },
     {
       name : 'Light',
@@ -152,7 +231,8 @@ export class MembersComponent implements OnInit {
   }
 
   onLoadMoreClick(){
-    this.loadMoreCount = this.members.length;
+    //this.loadMoreCount = this.members.length;
+    this.members = this.allMembers;
   }
 
 }
