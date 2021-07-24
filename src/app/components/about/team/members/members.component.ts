a6aae68265bf8eeefdb03706fd29a7f7566e9286
furkanksl <furkankoseoglu65@gmail.com>
2021-07-24 19:29:20 +0300

    added new team member

--- src/app/components/about/team/members/members.component.ts
+++ src/app/components/about/team/members/members.component.ts
@@ -6,21 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./members.component.scss']
 })
 export class MembersComponent implements OnInit {
-  loadMoreCount:number = 8;
+  loadMoreCount: number = 8;
 
-  members : any = [
+  members: any = [
     {
-      name : 'c0ntrol zer0',
-      image : 'assets/media/images/members/c0ntrolzer0.png',
-      title : 'Main Developer',
-      about : 'Software Engineer, SCRUM master and guardian of the keys.'
+      name: 'c0ntrol zer0',
+      image: 'assets/media/images/members/c0ntrolzer0.png',
+      title: 'Main Developer',
+      about: 'Software Engineer, SCRUM master and guardian of the keys.'
     },
     {
-      name : 'luisqsm',
-      image : 'assets/media/images/members/luisqsm.jpg',
-      title : 'Design & Brand Manager',
-      about : 'Architect, 3D artist, digital designer & crypto enthusiast.',
-      social : [
+      name: 'luisqsm',
+      image: 'assets/media/images/members/luisqsm.jpg',
+      title: 'Design & Brand Manager',
+      about: 'Architect, 3D artist, digital designer & crypto enthusiast.',
+      social: [
         {
           link: 'https://twitter.com/luisqsm2',
           icon: 'assets/media/icons/social/twitter-yellow-round-icon.svg',
@@ -29,11 +29,11 @@ export class MembersComponent implements OnInit {
       ]
     },
     {
-      name : 'Smack',
-      image : 'assets/media/images/members/smack.jpg',
-      title : 'Positivity Guru',
-      about : 'Connecting the people, the dots, to one piece of art.',
-      social :[
+      name: 'Smack',
+      image: 'assets/media/images/members/smack.jpg',
+      title: 'Positivity Guru',
+      about: 'Connecting the people, the dots, to one piece of art.',
+      social: [
         {
           link: 'https://twitter.com/smack_tm',
           icon: 'assets/media/icons/social/twitter-yellow-round-icon.svg',
@@ -47,29 +47,36 @@ export class MembersComponent implements OnInit {
       ]
     },
     {
-      name : 'Winnie',
-      image : 'assets/media/images/members/winnie.jpg',
-      title : 'Moderator/Contributor',
-      about : 'Running MoonSquad merchandise.'
+      name: 'Winnie',
+      image: 'assets/media/images/members/winnie.jpg',
+      title: 'Moderator/Contributor',
+      about: 'Running MoonSquad merchandise.'
     },
     {
-      name : 'Clover',
-      image : 'assets/media/images/members/clover.jpg',
-      title : 'Full stack expert',
-      about : 'Passionate and experienced frontend/backend/blockchain developer.'
+      name: 'CryptoSeas',
+      image: 'assets/media/images/members/cryptoseas.jpg',
+      title: 'Moderator/Contributor',
+      about: 'The voice of Moonshot.<br>Legacy Mod since day 1.',
+      social: [
+        {
+          link: 'https://twitter.com/TheVoiceOfCryp1',
+          icon: 'assets/media/icons/social/twitter-yellow-round-icon.svg',
+          alt: 'twitter-icon'
+        }
+      ]
     },
     {
-      name : 'CryptoKam',
-      image : 'assets/media/images/members/cryptokam.jpeg',
-      title : 'Moderator/Contributor',
-      about : 'Marketing advisor.<br>Creative ideas support.<br>Shill trooper.<br>'
+      name: 'CryptoKam',
+      image: 'assets/media/images/members/cryptokam.jpeg',
+      title: 'Moderator/Contributor',
+      about: 'Marketing advisor.<br>Creative ideas support.<br>Shill trooper.<br>'
     },
     {
-      name : 'CryptoHunter',
-      image : 'assets/media/images/members/cryptohunter.jpg',
-      title : 'Moderator/Marketeer', //'CryptoHunter, [Moderator/Marketeer]',
-      about : 'Digital Marketing SMO (Youtube, Instagram).',
-      social : [
+      name: 'CryptoHunter',
+      image: 'assets/media/images/members/cryptohunter.jpg',
+      title: 'Moderator/Marketeer', //'CryptoHunter, [Moderator/Marketeer]',
+      about: 'Digital Marketing SMO (Youtube, Instagram).',
+      social: [
         {
           link: 'https://twitter.com/cryptomshot/',
           icon: 'assets/media/icons/social/twitter-yellow-round-icon.svg',
@@ -78,22 +85,22 @@ export class MembersComponent implements OnInit {
       ]
     },
     {
-      name : 'Sonic',
-      image : 'assets/media/images/members/sonic.jpg',
-      title : 'Solidity Developer',
-      about : 'Experienced backend engineer turned solidity developer. Enthusiast of blockchain and smart contract.'
-      
+      name: 'Sonic',
+      image: 'assets/media/images/members/sonic.jpg',
+      title: 'Solidity Developer',
+      about: 'Experienced backend engineer turned solidity developer. Enthusiast of blockchain and smart contract.'
+
     }
   ];
 
 
-  otherMembers : any = [
+  otherMembers: any = [
     {
-      name : 'Majestic Drama',
-      image : 'assets/media/images/members/majestic.png',
-      title : 'Community Ambassador', 
-      about : 'Music producer & songwriter who has produced tracks for Wale, Kid Ink, Fetty Wap, Twista, and La Fouine.',
-      social : [
+      name: 'Majestic Drama',
+      image: 'assets/media/images/members/majestic.png',
+      title: 'Community Ambassador',
+      about: 'Music producer & songwriter who has produced tracks for Wale, Kid Ink, Fetty Wap, Twista, and La Fouine.',
+      social: [
         {
           link: 'https://twitter.com/MajesticDrama',
           icon: 'assets/media/icons/social/twitter-yellow-round-icon.svg',
@@ -107,25 +114,25 @@ export class MembersComponent implements OnInit {
       ]
     },
     {
-      name : 'Light',
-      image : 'assets/media/images/members/light.jpg',
-      title : 'Fullstack Developer',
-      about : 'Panda on steroids with over 16 years experience in developing modern apps & Pro Google & Bio hacker.'
-      
+      name: 'Light',
+      image: 'assets/media/images/members/light.jpg',
+      title: 'Fullstack Developer',
+      about: 'Panda on steroids with over 16 years experience in developing modern apps & Pro Google & Bio hacker.'
+
     },
     {
-      name : 'Furkan',
-      image : 'assets/media/images/members/furkan.png',
-      title : 'Mobile Developer',
-      about : 'A person who likes to produce something and learn quickly.'
-      
+      name: 'Furkan',
+      image: 'assets/media/images/members/furkan.png',
+      title: 'Mobile Developer',
+      about: 'A person who likes to produce something and learn quickly.'
+
     },
     {
-      name : 'Moon Squad',
-      image : 'assets/media/images/members/moonsquad.jpg',
-      title : 'Moderator/Marketeer ',
-      about : 'Social Media Director & Digital Marketing - SMO.',
-      social : [
+      name: 'Moon Squad',
+      image: 'assets/media/images/members/moonsquad.jpg',
+      title: 'Moderator/Marketeer ',
+      about: 'Social Media Director & Digital Marketing - SMO.',
+      social: [
         {
           link: 'https://twitter.com/Mo0NsQuadguys',
           icon: 'assets/media/icons/social/twitter-yellow-round-icon.svg',
@@ -134,25 +141,31 @@ export class MembersComponent implements OnInit {
       ]
     },
     {
-      name : 'Rick',
-      image : 'assets/media/images/members/rick.jpeg',
-      title : 'Moderator & Discord Guru',
-      about : 'Marketing & hype contributor.<br>Moonshot lover since day 1.'
-      
+      name: 'Clover',
+      image: 'assets/media/images/members/clover.jpg',
+      title: 'Full stack expert',
+      about: 'Passionate and experienced frontend/backend/blockchain developer.'
     },
     {
-      name : 'Chocoboknight1',
-      image : 'assets/media/images/members/chocoboknight1.png',
-      title : 'Community Manager',
-      about : 'With 10 years of team leading experience, working to grow the project\'s presence on Reddit and other media platforms.',
-      
+      name: 'Rick',
+      image: 'assets/media/images/members/rick.jpeg',
+      title: 'Moderator & Discord Guru',
+      about: 'Marketing & hype contributor.<br>Moonshot lover since day 1.'
+
     },
     {
-      name : 'Meenu',
-      image : 'assets/media/images/members/shef.jpg',
-      title : 'Moderator/Contributor',
-      about : 'Monitoring, tracking and engaging with contents in the community and responding their queries in a timely manner.',
-      social : [
+      name: 'Chocoboknight1',
+      image: 'assets/media/images/members/chocoboknight1.png',
+      title: 'Community Manager',
+      about: 'With 10 years of team leading experience, working to grow the project\'s presence on Reddit and other media platforms.',
+
+    },
+    {
+      name: 'Meenu',
+      image: 'assets/media/images/members/shef.jpg',
+      title: 'Moderator/Contributor',
+      about: 'Monitoring, tracking and engaging with contents in the community and responding their queries in a timely manner.',
+      social: [
         {
           link: 'https://twitter.com/meenubeniwal',
           icon: 'assets/media/icons/social/twitter-yellow-round-icon.svg',
@@ -161,10 +174,10 @@ export class MembersComponent implements OnInit {
       ]
     },
     {
-      name : 'Ironlionzion',
-      image : 'assets/media/images/members/ironlionzion.jpg',
-      title : 'Moderator/Contributor',
-      about : 'Professional shiller.<br>Marketing ideas contributor.<br>'
+      name: 'Ironlionzion',
+      image: 'assets/media/images/members/ironlionzion.jpg',
+      title: 'Moderator/Contributor',
+      about: 'Professional shiller.<br>Marketing ideas contributor.<br>'
     }
   ];
 
@@ -173,7 +186,7 @@ export class MembersComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onLoadMoreClick(){
+  onLoadMoreClick() {
     //this.loadMoreCount = this.members.length;
     this.members.push(...this.otherMembers);
   }
