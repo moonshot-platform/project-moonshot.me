c7ffef6db065d3783fb7205132630b6cf456cbc3
furkanksl <furkankoseoglu65@gmail.com>
2021-08-12 12:08:40 +0300

    added press details in press section

--- src/app/components/landing/press/press.component.ts
+++ src/app/components/landing/press/press.component.ts
@@ -9,34 +9,29 @@ export class PressComponent implements OnInit {
 
   pressList: any[] = [
     {
-      'img': 'assets/media/images/press/Forbes_placeholder_yellow.svg',
-      'whiteImg': 'assets/media/images/press/Forbes_placeholder_white.svg',
+      'img': 'assets/media/images/press/Investorplace_yellow.svg',
+      'whiteImg': 'assets/media/images/press/Investorplace_white.svg',
+      'link': 'https://investorplace.com/2021/05/moonshot-crypto-11-things-to-know-about-the-new-token-driving-social-media-chatter/',
       hovered: false,
     },
     {
-      'img': 'assets/media/images/press/Forbes_placeholder_yellow.svg',
-      'whiteImg': 'assets/media/images/press/Forbes_placeholder_white.svg',
+      'img': 'assets/media/images/press/Influencive_yellow.svg',
+      'whiteImg': 'assets/media/images/press/Influencive_white.svg',
+      'link': 'https://www.influencive.com/we-interviewed-developer-of-a-new-cryptocurrency-moonshot-to-find-out-many-interesting-things/',
       hovered: false,
     },
     {
-      'img': 'assets/media/images/press/U-Today_placeholder_yellow.svg',
-      'whiteImg': 'assets/media/images/press/U-Today_placeholder_white.svg',
-      hovered: false,
+      'img': 'assets/media/images/press/Markets-insider_yellow.svg',
+      'whiteImg': 'assets/media/images/press/Markets-insider_white.svg',
+      'link': 'https://markets.businessinsider.com/news/stocks/moonshot-crypto-11-things-to-know-about-the-new-token-driving-social-media-chatter-1030403285',
+      hovered: false
     },
     {
-      'img': 'assets/media/images/press/U-Today_placeholder_yellow.svg',
-      'whiteImg': 'assets/media/images/press/U-Today_placeholder_white.svg',
-      hovered: false,
-    },
-    {
-      'img': 'assets/media/images/press/Yahoo_placeholder_yellow.svg',
-      'whiteImg': 'assets/media/images/press/Yahoo_placeholder_white.svg',
-      hovered: false,
-    },
-    {
-      'img': 'assets/media/images/press/Yahoo_placeholder_yellow.svg',
-      'whiteImg': 'assets/media/images/press/Yahoo_placeholder_white.svg',
-      hovered: false,
+      'img': 'assets/media/images/press/Einpresswire_yellow.svg',
+      'whiteImg': 'assets/media/images/press/Einpresswire_white.svg',
+      'link': 'https://www.einnews.com/pr_news/544177272/space-themed-nft-platform-moonshot-revamps-website-with-a-fresh-look',
+      hovered: false
+
     }
   ];
 
