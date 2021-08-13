e660559ddd5d55e6fe1408b60e016fc219326d0a
furkanksl <furkankoseoglu65@gmail.com>
2021-08-13 19:34:58 +0300

    MoonbaseService implemented to sidebar  component

--- src/app/components/base/sidebar/sidebar.component.ts
+++ src/app/components/base/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TokenomicsService } from 'src/app/services/tokenomics.service';
+import { MoonbaseService } from 'src/app/services/moonbase.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -9,16 +10,28 @@ import { TokenomicsService } from 'src/app/services/tokenomics.service';
 export class SidebarComponent implements OnInit {
 
   active = false;
+  moonbaseActive = false;
 
-  constructor(private tokenomicsService: TokenomicsService) { }
+  constructor(
+    private tokenomicsService: TokenomicsService,
+    private moonbaseService: MoonbaseService
+  ) { }
 
   ngOnInit(): void {
-    this.tokenomicsService.whenToggled().subscribe((state:boolean) => {
+    this.tokenomicsService.whenToggled().subscribe((state: boolean) => {
+      this.toggleTokenomicsView(state);
+    });
+
+    this.moonbaseService.whenToggled().subscribe((state: boolean) => {
       this.toggleTokenomicsView(state);
     });
   }
 
-  toggleTokenomicsView( active: boolean = null ) {
+  toggleTokenomicsView(active: boolean = null) {
     this.active = active || !this.active;
+  }
+
+  toggleMoonbaseService(moonbaseActive: boolean = null) {
+    this.moonbaseActive = moonbaseActive || !this.moonbaseActive;
   }
 }
