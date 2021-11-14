333781cec84f353d0b05606944dff99aa652748d
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2021-11-14 23:24:59 +0300

    added night sky bg template

--- src/app/components/night-sky/night-sky.component.ts
+++ src/app/components/night-sky/night-sky.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./night-sky.component.scss']
 })
 export class NightSkyComponent implements OnInit {
+  static readonly routeName: string = 'night-sky';
 
   constructor() { }
 
