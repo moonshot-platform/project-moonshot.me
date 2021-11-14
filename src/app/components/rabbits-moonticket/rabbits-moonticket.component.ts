a81204bbe199e5597dbd7980b103c52167941f32
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2021-11-14 23:25:54 +0300

    created rabbits moonticket component

---
+++ src/app/components/rabbits-moonticket/rabbits-moonticket.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rabbits-moonticket',
+  templateUrl: './rabbits-moonticket.component.html',
+  styleUrls: ['./rabbits-moonticket.component.scss']
+})
+export class RabbitsMoonticketComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
