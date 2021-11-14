0f75ad3a95131bbf2f673bae045c681121326337
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2021-11-14 23:21:11 +0300

    created night sky component

---
+++ src/app/components/night-sky/night-sky.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-night-sky',
+  templateUrl: './night-sky.component.html',
+  styleUrls: ['./night-sky.component.scss']
+})
+export class NightSkyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
