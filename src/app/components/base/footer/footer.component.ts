b6ec832bbfcdaf4e2a33e3432817ca9177675f34
S1nPur1ty <singular.purity@gmail.com>
2021-12-03 22:22:18 +0800

    updated all the pcs urls

--- src/app/components/base/footer/footer.component.ts
+++ src/app/components/base/footer/footer.component.ts
@@ -112,7 +112,7 @@ export class FooterComponent implements OnInit {
   scrollToElement(page: string, fragment: string): void {
     const element = document.querySelector(`#${fragment}`)
     if (element) {
-      element.scrollIntoView({ behavior: 'smooth', block: 'start' })
+      setTimeout(() => element.scrollIntoView({ behavior: 'smooth', block: 'start' }), 100);
     } else
       this._router.navigate([page], { fragment: fragment });
   }
--- src/app/components/news/article-list/article-list.component.ts
+++ src/app/components/news/article-list/article-list.component.ts
@@ -194,7 +194,7 @@ export class ArticleListComponent implements OnInit {
   scrollToElement(page: string, fragment: string): void {
     const element = document.querySelector(`#${fragment}`)
     if (element) {
-      element.scrollIntoView({ behavior: 'smooth', block: 'start' })
+      setTimeout(() => element.scrollIntoView({ behavior: 'smooth', block: 'start' }), 100);
     }
   }
 
