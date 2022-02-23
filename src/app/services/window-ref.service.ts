7153501c4efbe025b330c7543fe4a202199ca133
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-02-23 17:08:26 +0300

    Create a service for Window operations

---
+++ src/app/services/window-ref.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+function _window(): any {
+    return window;
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class WindowRefService {
+    get nativeWindow(): any {
+        return _window();
+    }
+}
