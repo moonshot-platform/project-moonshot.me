d9b7064f5d098957b118ff1ff2af02c6922591f3
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-03-29 21:50:50 +0300

    Add import token functionality to wallet service

--- src/app/services/wallet-service.service.ts
+++ src/app/services/wallet-service.service.ts
@@ -327,4 +327,35 @@ export class WalletService {
       this.toastrService.error('Operation Failed!')
     }
   }
+
+
+  addTokenMshotToMetaMaskWallet() {
+    const tokenAddress = tokenContractAddress;
+    const tokenSymbol = 'MSHOT';
+    const tokenDecimals = 18;
+    const tokenImage = 'https://project-moonshot.me/assets/media/images/logo.png';
+
+    if (typeof window.ethereum !== 'undefined' || (typeof window.web3 !== 'undefined')) {
+      try {
+        // wasAdded is a boolean. Like any RPC method, an error may be thrown.
+        this.windowRef.nativeWindow.ethereum.request({
+          method: 'wallet_watchAsset',
+          params:
+          {
+            type: 'ERC20', // Initially only supports ERC20, but eventually more!
+            options: {
+              address: tokenAddress, // The address that the token is at.
+              symbol: tokenSymbol, // A ticker symbol or shorthand, up to 5 chars.
+              decimals: tokenDecimals, // The number of decimals in the token
+              image: tokenImage // A string url of the token logo
+            }
+          },
+        });
+
+        location.reload()
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
 }
