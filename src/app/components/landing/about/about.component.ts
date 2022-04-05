70a0b58b1f0766f5e8f2bd0a950f23dfef37560a
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-04-05 14:20:49 +0300

    Update About section in Landing page

--- src/app/components/landing/about/about.component.ts
+++ src/app/components/landing/about/about.component.ts
@@ -62,4 +62,11 @@ export class AboutComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  scrollToElement(page: string, fragment: string): void {
+    const element = document.querySelector(`#${fragment}`)
+    if (element) {
+      setTimeout(() => element.scrollIntoView({ behavior: 'smooth', block: 'start' }), 500);
+    }
+  }
+
 }
