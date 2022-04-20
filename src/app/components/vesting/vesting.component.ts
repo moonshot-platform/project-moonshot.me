5c4edf3d742f9552ed84c1ffbdc5d3d55a7cdf7f
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-04-20 03:40:45 +0300

    Add get user vesting data by searching

--- src/app/components/vesting/vesting.component.ts
+++ src/app/components/vesting/vesting.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ToastrService } from 'ngx-toastr';
+import { WalletService } from 'src/app/services/wallet-service.service';
+import { WalletConnectComponent } from '../base/wallet-connect/wallet-connect.component';
 
 @Component({
   selector: 'app-vesting',
@@ -16,14 +20,90 @@ export class VestingComponent implements OnInit {
   isRevocable: boolean = false;
 
   search: string = '';
+  userVestingData: any
 
-  constructor() { }
+  isConnected: boolean = false;
+  isOwner: boolean = false;
+
+  constructor(
+    private walletConnectService: WalletService,
+    private dialog: MatDialog,
+    private toastrService: ToastrService,
+  ) {
+    this.walletConnectService.init().then(async (data: boolean) => {
+      this.isConnected = data;
+      if (data) {
+        this.isOwner = await this.walletConnectService.isOwner();
+      }
+
+      this.walletConnectService.setWalletState(this.isConnected);
+    });
+  }
 
   ngOnInit(): void {
+    this.walletConnectService.onWalletStateChanged().subscribe(async (state: boolean) => {
+      this.isConnected = state;
+    });
   }
 
   changeRevocableStatus(state: boolean) {
     this.isRevocable = state;
   }
 
+  async createNewSchedule() {
+    if (this.isConnected) {
+      if (this.isOwner) {
+        await this.walletConnectService.createVestingSchedule(
+          this.beneficiary,
+          this.cliff,
+          this.duration,
+          this.isRevocable,
+          this.amount,
+        )
+      } else {
+        this.toastrService.error("You are not an owner!");
+      }
+
+      this.clearForm();
+    } else {
+      this.openWalletConnectionDialog();
+    }
+  }
+
+  clearForm() {
+    this.beneficiary = "";
+    this.amount = 0;
+    this.cliff = 0;
+    this.duration = 0;
+    this.isRevocable = false;
+  }
+
+  clearSearch() {
+    this.search = "";
+    this.userVestingData = undefined;
+  }
+
+  async searchAddress() {
+
+    if (!this.isConnected) {
+      this.openWalletConnectionDialog();
+    } else if (this.search !== "") {
+      this.userVestingData = await this.walletConnectService.searchLastVestingScheduleForHolder(this.search);
+
+      if (this.userVestingData !== undefined) {
+        this.beneficiary = this.userVestingData.beneficiary;
+        this.amount = this.userVestingData.amountTotal;
+        this.duration = this.userVestingData.duration;
+        this.isRevocable = this.userVestingData.revocable;
+        this.cliff = this.userVestingData.cliff;
+      }
+    }
+  }
+
+  openWalletConnectionDialog(): void {
+    let dialogRef = this.dialog.open(
+      WalletConnectComponent,
+      { width: 'auto' }
+    );
+  }
 }
