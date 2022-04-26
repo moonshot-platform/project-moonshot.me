458c2147d0b015ba7446da792f13c1307416e751
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-04-26 17:13:26 +0300

    Update vesting bar for multiple vesting token

--- src/app/components/base/release-bar/release-bar.component.ts
+++ src/app/components/base/release-bar/release-bar.component.ts
@@ -2,7 +2,7 @@ import { Component, HostListener, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ToastrService } from 'ngx-toastr';
 import { ReleaseService } from 'src/app/services/release.service';
-import { WalletService } from 'src/app/services/wallet-service.service';
+import { WalletService, VESTING_CONTRACTS } from 'src/app/services/wallet-service.service';
 import { environment } from 'src/environments/environment';
 import { WalletConnectComponent } from '../wallet-connect/wallet-connect.component';
 
@@ -13,14 +13,12 @@ import { WalletConnectComponent } from '../wallet-connect/wallet-connect.compone
 })
 export class ReleaseBarComponent implements OnInit {
 
-  items: DropdownModel[] = [
-    new DropdownModel(environment.vestingContactAddress, "MSHOT"),
-    new DropdownModel(environment.rabbitContractAddress, "RA8BIT")
-  ]
+
+  items: any[] = [];
 
   isDropdownActive: boolean = false;
 
-  selectedItem: DropdownModel = this.items[0];
+  selectedItem;
 
   private userData: any;
 
@@ -44,31 +42,41 @@ export class ReleaseBarComponent implements OnInit {
     private walletConnectService: WalletService,
     private toastrService: ToastrService,
     private dialog: MatDialog
-  ) { }
+  ) {
+
+    for (const key in VESTING_CONTRACTS) {
+      this.items.push(VESTING_CONTRACTS[key]);
+    }
+
+    this.selectedItem = this.items[0];
+  }
 
   ngOnInit(): void {
-    this.walletConnectService.init().then(async (data: boolean) => {
-      this.isConnected = data;
-      if (data) {
-        // console.log("Wallet is Connected");
-        this.getBnbBalance();
-        this.computeReleasableAmount();
-        this.checkUserVested();
-      }
-
-      this.walletConnectService.setWalletState(this.isConnected);
-    });
 
     this.walletConnectService.onWalletStateChanged().subscribe(async (state: boolean) => {
       this.isConnected = state;
 
       if (state) {
         this.hasClaimed = await this.walletConnectService.hasClaimed();
-        this.computeReleasableAmount();
-        this.checkUserVested();
-        this.getBnbBalance();
+        await this.computeReleasableAmount();
+        await this.checkUserVested();
+        await this.getBnbBalance();
       }
     });
+
+    this.walletConnectService.init().then(async (data: boolean) => {
+      this.isConnected = data;
+      if (data) {
+        // console.log("Wallet is Connected");
+        // await this.computeReleasableAmount();
+        // await this.checkUserVested();
+        // await this.getBnbBalance();
+      }
+
+      this.walletConnectService.setWalletState(this.isConnected);
+    });
+
+
   }
 
   toggleDropdown() {
@@ -79,10 +87,11 @@ export class ReleaseBarComponent implements OnInit {
     this.releaseService.onToggle(false);
   }
 
-  async selectItem(item: DropdownModel) {
+  async selectItem(item: any) {
     this.selectedItem = item;
     this.toggleDropdown();
-    // console.log(await this.walletConnectService.hasVested());
+    await this.checkUserVested();
+    // console.log(await this.checkUserVested());
   }
 
   @HostListener('document:click', ['$event'])
@@ -102,12 +111,10 @@ export class ReleaseBarComponent implements OnInit {
   }
 
   async computeReleasableAmount() {
-
-    this.releasableAmount = await this.walletConnectService.computeReleasableAmount();
+    this.releasableAmount = await this.walletConnectService.computeReleasableAmount(this.selectedItem);
     // 1T = 1 Trillion, 1B = 1 Billion, 1M = 1 Million , values smaller can be displayed as is
-    // console.log(this.abbreviateNumber(parseInt(this.releasableAmount.toString())));
     this.releasableAmount = this.walletConnectService.shortTheNumber(this.releasableAmount);
-    // console.log(this.walletConnectService.shortTheNumber(this.releasableAmount));
+    console.log(this.walletConnectService.shortTheNumber(this.releasableAmount));
   }
 
   async release() {
@@ -116,7 +123,7 @@ export class ReleaseBarComponent implements OnInit {
 
       this.isInReleasingProcess = true;
       if (this.hasEnoughBnb) {
-        await this.walletConnectService.releaseVesting();
+        await this.walletConnectService.releaseVesting(this.selectedItem);
         this.hasEnoughBnb = false
       } else {
         this.toastrService.error("You do not have enough bnb to pay the gas fee!")
@@ -129,13 +136,14 @@ export class ReleaseBarComponent implements OnInit {
   }
 
   async checkUserVested() {
-    this.hasVested = await this.walletConnectService.hasVested();
+    this.hasVested = await this.walletConnectService.hasVested(this.selectedItem);
+    return this.hasVested;
   }
 
   checkBNBBalance = async () => this.hasEnoughBnb = await this.walletConnectService.checkBnbBalance();
 
   async onChangeBuyMSHOTInput(value: any) {
-    let bnbAmount = this.bnbCountFromInput <= 0 ? 0.001 : this.bnbCountFromInput;
+    this.bnbCountFromInput = this.bnbCountFromInput <= 0 ? 0.001 : this.bnbCountFromInput;
     // console.log(bnbAmount);
 
     if (value + this.estimatedGasFee > this.bnbBalance) {
@@ -153,12 +161,3 @@ export class ReleaseBarComponent implements OnInit {
   }
 }
 
-class DropdownModel {
-  tokenAddress: string;
-  tokenSymbol: string;
-
-  constructor(address: string, symbol: string) {
-    this.tokenAddress = address;
-    this.tokenSymbol = symbol;
-  }
-}
