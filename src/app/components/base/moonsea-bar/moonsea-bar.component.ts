f74c94cbb24eacc72b9a3f53fcbcdeb39847706b
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-05-16 01:40:03 +0300

    Create a component for MoonSea sidebar

---
+++ src/app/components/base/moonsea-bar/moonsea-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-moonsea-bar',
+  templateUrl: './moonsea-bar.component.html',
+  styleUrls: ['./moonsea-bar.component.scss']
+})
+export class MoonseaBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
