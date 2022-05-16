a69c2280e71a4f0c89032c047e2b4ab162881b73
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-05-16 09:35:52 +0300

    Add wallet address section to Moonbase bar

--- src/app/components/base/moonbase-bar/moonbase-bar.component.ts
+++ src/app/components/base/moonbase-bar/moonbase-bar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MoonbaseService } from 'src/app/services/moonbase.service';
+import { WalletService } from 'src/app/services/wallet-service.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-moonbase-bar',
@@ -7,13 +9,46 @@ import { MoonbaseService } from 'src/app/services/moonbase.service';
   styleUrls: ['./moonbase-bar.component.scss']
 })
 export class MoonbaseBarComponent implements OnInit {
-  constructor(private moonbaseService: MoonbaseService) { }
+  address: string = '';
+  isConnected: boolean = false;
+  userData: any;
+  shortenedWalletAddress: string = ''
+
+  constructor(
+    private moonbaseService: MoonbaseService,
+    private walletService: WalletService,
+  ) {
+    this.walletService.init().then((data: boolean) => {
+      this.isConnected = data;
+    });
+  }
 
   ngOnInit(): void {
+    this.walletService.getData().subscribe((data: any) => {
+      this.userData = data;
+      if (data !== undefined && data.address != undefined) {
+        if (this.userData.networkId.chainId == environment.chainId) {
+          this.isConnected = true;
+          this.address = data.address;
+          this.shortenedWalletAddress = this.shortWalletAddress();
+        } else {
+          this.address = '';
+          this.shortenedWalletAddress = '';
+        }
+      }
+    });
+
+    this.walletService.onWalletStateChanged().subscribe((state: boolean) => {
+      this.isConnected = state
+    });
+
+  }
+
+  shortWalletAddress(): string {
+    return this.address.slice(0, this.address.length / 2) + '...' + this.address.slice(-((this.address.length / 2) - 9))
   }
 
   toggleMoonbase(): void {
     this.moonbaseService.onToggle(false);
   }
-
 }
