7f98a5b0b1b3b41206182f114d2ab43c4022a375
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-05-19 15:51:55 +0300

    Add faucet functionality to service

--- src/app/components/faucet/faucet.component.ts
+++ src/app/components/faucet/faucet.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ToastrService } from 'ngx-toastr';
 import { WalletService } from 'src/app/services/wallet-service.service';
+import { WalletConnectComponent } from '../base/wallet-connect/wallet-connect.component';
 
 @Component({
   selector: 'app-faucet',
@@ -9,11 +12,80 @@ import { WalletService } from 'src/app/services/wallet-service.service';
 export class FaucetComponent implements OnInit {
   static readonly routeName: string = 'faucet';
 
+  search: any;
+  isConnected: boolean = false;
+  connectedAddress: string = '';
+  canUserWithdraw: boolean = false;
+  cooldown: number = 0;
+  isInProcess: boolean = false;
+  currentBalance: number = 0;
+
+  tokenAmount = '';
+
   constructor(
     private walletService: WalletService,
-  ) { }
+    private dialog: MatDialog,
+    private toastrService: ToastrService,
+  ) {
+    this.walletService.init().then(async (data: boolean) => {
+      this.isConnected = data;
+      if (data) {
+        this.connectedAddress = this.walletService.account;
+        this.tokenAmount = await this.walletService.getFaucetAmount();
+        this.canUserWithdraw = await this.walletService.canWithdrawOnFaucet();
 
-  ngOnInit(): void {
+        this.currentBalance = this.walletService.shortTheNumber(await this.walletService.getUserMSHOTBalance(this.connectedAddress) as number)
+
+        if (!this.canUserWithdraw) {
+          this.cooldown = await this.walletService.getRemainingCoolDownForFaucet();
+          this.startToCountdownTime();
+        }
+      }
+      this.walletService.setWalletState(this.isConnected);
+    });
   }
 
+  ngOnInit(): void {
+    this.walletService.onWalletStateChanged().subscribe(async (state: boolean) => {
+      this.isConnected = state;
+      if (state) {
+        this.connectedAddress = this.walletService.account;
+      } else {
+        this.connectedAddress = '';
+      }
+    });
+  }
+
+  connectWallet(): void {
+    let dialogRef = this.dialog.open(
+      WalletConnectComponent,
+      { width: 'auto' }
+    );
+  }
+
+  async getFaucet() {
+    this.isInProcess = true;
+
+    if (this.isConnected) {
+
+      if (this.canUserWithdraw) {
+        console.log(await this.walletService.getFreeMoonshot());
+      } else {
+        this.startToCountdownTime();
+        this.toastrService.info("You already got Free Moonshot!", "FAUCET")
+      }
+
+      this.canUserWithdraw = await this.walletService.canWithdrawOnFaucet();
+    } else {
+      this.connectWallet();
+    }
+
+    this.isInProcess = false;
+  }
+
+  startToCountdownTime() {
+    setInterval(() => {
+      this.cooldown -= 1;
+    }, 1000);//run this thang every 1 seconds
+  }
 }
