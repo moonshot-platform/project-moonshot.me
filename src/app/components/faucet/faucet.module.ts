df37d9d334048cc467e1dd9465878ca7c73f00c9
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-05-19 15:51:35 +0300

    Update Faucet page layout

--- src/app/components/faucet/faucet.module.ts
+++ src/app/components/faucet/faucet.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FaucetComponent } from './faucet.component';
 import { NavigationModule } from '../base/navigation/navigation.module';
+import { FormsModule } from '@angular/forms';
+import { SidebarModule } from '../base/sidebar/sidebar.module';
 
 
 
@@ -11,8 +13,9 @@ import { NavigationModule } from '../base/navigation/navigation.module';
   ],
   imports: [
     CommonModule,
-
+    FormsModule,
     NavigationModule,
+    SidebarModule
   ]
 })
 export class FaucetModule { }
