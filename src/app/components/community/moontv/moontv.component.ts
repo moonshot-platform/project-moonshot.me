9101766f0fd51fce6c7a23a931ada212aa7b2c3b
Moonshot <>
2023-03-14 01:30:15 +0100

    update moontv

--- src/app/components/community/moontv/moontv.component.ts
+++ src/app/components/community/moontv/moontv.component.ts
@@ -10,59 +10,13 @@ export class MoontvComponent implements OnInit {
 
   public videoSrcList = [
     {
-      src: this.transform("https://www.youtube.com/embed/zPpOdMsC2Wk"),
+      src: this.transform("https://www.youtube.com/watch?v=uRi-6OL-QrI"),
       thumb: ""
     },
     {
-      src: this.transform("https://www.youtube.com/embed/yx8x7DtBXWs"),
+      src: this.transform("https://www.youtube.com/watch?v=DmjcRE0NP0w"),
       thumb: ""
-    },
-    {
-      src: this.transform("https://www.youtube.com/embed/XyBA15bko2o"),
-      thumb: ""
-    },
-    {
-      src: this.transform("https://www.youtube.com/embed/_yBjjzwxXlg"),
-      thumb: ""
-    },
-    {
-      src: this.transform("https://www.youtube.com/embed/uRi-6OL-QrI"),
-      thumb: ""
-    },
-    {
-      src: this.transform("https://www.youtube.com/embed/SnsdjLtS33I"),
-      thumb: ""
-    },
-    {
-      src: this.transform("https://www.youtube.com/embed/Smzc1bmfkrc"),
-      thumb: ""
-    },
-    {
-      src: this.transform("https://www.youtube.com/embed/EzoCAroGtxw"),
-      thumb: ""
-    },
-    {
-      src: this.transform("https://www.youtube.com/embed/k-3vRPmZpU0"),
-      thumb: ""
-    },
-    {
-      src: this.transform("https://www.youtube.com/embed/u5vn5c2lja0"),
-      thumb: ""
-    },
-    ///
-    {
-      src: this.transform("https://www.youtube.com/embed/O54ASHsKso0"),
-      thumb: ""
-    },
-    {
-      src: this.transform("https://www.youtube.com/embed/5WaTUWeeDxE"),
-      thumb: ""
-    },
-    // {
-    //   src: this.transform("https://www.youtube.com/embed/EzoCAroGtxw"),
-    //   thumb: ""
-    // },
-
+    }
   ];
 
   constructor(private sanitizer: DomSanitizer) { }
